c02bda0d126c5046fd457db1ce08ea7d607f1222
EP-coode <68182419+EP-coode@users.noreply.github.com>
2021-08-22 12:19:30 +0200

    fix particles

--- js/particles.js
+++ js/particles.js
@@ -13,27 +13,46 @@ function fitToContainer(canvas) {
     canvas.height = canvas.offsetHeight;
 }
 
-// from chrome 64
+// to make canvas always fit div/banner
 new ResizeObserver(() => fitToContainer(canvas)).observe(banner)
 
 // ======================== particles ========================
 
-// globals
+const PARTICLE_MAX_SIZE = 15
+const STARVE_RATE = 0.2
+const MOUSE_FORCE_FACTOR = -0.005
+const INITIAL_SPEED_FACTOR = 0.2
+const PARTICLES_AMMOUT = 60
+// forve of enviroment applied to evry particle
+const ENV_FORCE = Object.freeze({x: 0, y: 0.0005})
+
+// colors
+const HUE_START = 160
+const HUE_END = 200
+const HUE_STEP = 1
+
 const particles = []
+
 let deltaTime = 0
 let lastFrameTime = new Date().getTime()
+
+// mouse position relative to canvas
 const mouse = {
     x: null,
     y: null,
     dx: null,
     dy: null
 }
-let hue = (Math.random() * 255) % 30 + 160
+let hue = 0
 
+
+// determins how many deggres is betwen start and end on hue circel
+const hue_path = HUE_END > HUE_START ? HUE_END - HUE_START : 360 - HUE_START + HUE_END
 function nextColor() {
-    hue = (hue + 1) % 30 + 160
+    hue = ((hue + HUE_STEP) % hue_path + HUE_START) % 360
 }
 
+// inneficient
 window.addEventListener('mousemove', e => {
     const rect = canvas.getBoundingClientRect()
 
@@ -45,40 +64,42 @@ window.addEventListener('mousemove', e => {
 
 class Particle {
     constructor(color = '#FF2233') {
-        this.position = { x: 10, y: 10 }
+        this.position = { x: 0, y: 0 }
         this.speed = { x: 0, y: 0 }
-        this.force = { x: 0, y: 0 }
-        this.starve_rate = 0.2
-        this.size = 15
+        this.force = { x: ENV_FORCE.x, y: ENV_FORCE.y }
+        this.starve_rate = STARVE_RATE
+        this.size = 0
         this.color = color
     }
 
+    // respawns particle at certain position
     respawn(x, y) {
         this.position = { x, y }
-        this.speed.x = 0.1 * (Math.random() * 2 - 1) * 2
-        this.speed.y = 0.1 * (Math.random() * 2 - 1) * 2
-        this.size = 15 * Math.random()
+        this.color = `hsla(${hue},100%,50%,0.5)`
+        nextColor()
+        this.speed.x = INITIAL_SPEED_FACTOR * (Math.random() * 2 - 1)
+        this.speed.y = INITIAL_SPEED_FACTOR * (Math.random() * 2 - 1)
+        this.size = PARTICLE_MAX_SIZE * Math.random()
     }
 
+    // determins if this particle is visable in canvas
     isVisable() {
         const { x, y } = this.position
-        const rect = canvas.getBoundingClientRect()
         return this.size > 0 &&
-            canvas.width > Math.max(x - rect.left, 0) &&
-            canvas.height > Math.max(y - rect.top, 0) &&
+            canvas.width > x &&
+            canvas.height > y &&
             x > 0 &&
             y > 0
     }
 
     update(deltaTime) {
         const { x, y } = this.position
-        // temporaty sollution
-        const r_sqr = Math.pow(mouse.x - x, 2) + Math.pow(mouse.y - y, 2)
-        const forceFactor = 0.01
-        this.force.x = forceFactor * (x - mouse.x) / Math.max(r_sqr, 10)
-        this.force.y = forceFactor * (y - mouse.y) / Math.max(r_sqr, 10)
 
-        this.speed.x += Math.min(this.force.x * deltaTime,)
+        const r_sqr = Math.pow(mouse.x - x, 2) + Math.pow(mouse.y - y, 2)
+        this.force.x = (MOUSE_FORCE_FACTOR * (x - mouse.x) / Math.max(r_sqr, 10)) + ENV_FORCE.x
+        this.force.y = (MOUSE_FORCE_FACTOR * (y - mouse.y) / Math.max(r_sqr, 10)) + ENV_FORCE.y
+
+        this.speed.x += this.force.x * deltaTime
         this.speed.y += this.force.y * deltaTime
         this.position.x += this.speed.x * deltaTime
         this.position.y += this.speed.y * deltaTime
@@ -100,12 +121,18 @@ class Particle {
     }
 }
 
-const rect = canvas.getBoundingClientRect()
-for (let i = 0; i < 50; i++) {
-    const particle = new Particle(`hsla(${hue},100%,50%,0.5)`)
-    nextColor()
-    particles.push(particle)
-    particle.respawn(Math.random() * rect.width, Math.random() * rect.height)
+function initialize()
+{
+    for (let i = 0; i < PARTICLES_AMMOUT; i++) {
+        const particle = new Particle(`hsla(${hue},100%,50%,0.5)`)
+        nextColor()
+        particles.push(particle)
+    }
+}
+
+function isMouseInCanvas(){
+    return mouse.x < canvas.width && mouse.x > 0 &&
+            mouse.y < canvas.height && mouse.y > 0
 }
 
 
@@ -113,20 +140,15 @@ function animate() {
     deltaTime = new Date().getTime() - lastFrameTime
     lastFrameTime = new Date().getTime()
 
-    // if (blur < 0)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // else {
-    //     ctx.fillStyle = `rgba(0,0,0,${1 - blur})`
-    //     ctx.fillRect(0, 0, canvas.width, canvas.height)
-    // }
 
     particles.forEach(p => p.update(deltaTime))
 
+    const _isMouseinCanvas = isMouseInCanvas()
+
     particles.forEach(p => {
-        if (!p.isVisable()) {
+        if (!p.isVisable() && _isMouseinCanvas) 
             p.respawn(mouse.x + Math.random() - 0.5, mouse.y + Math.random() - 0.5)
-            //p.respawn(mouse.x,mouse.y)
-        }
     })
 
     particles.forEach(p => p.draw())
@@ -134,5 +156,6 @@ function animate() {
     requestAnimationFrame(animate)
 }
 
+initialize()
 animate()
 
